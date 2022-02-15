2919a00928c05ef17acb5257cb1cefd1b36843a5
Ruslan Repin <ruslan_r@digann.ru>
2022-02-15 22:26:20 +0300

    Parser: Парсер файла.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("mbr");
+        Parser parser = new Parser();
+        parser.setFile(file);
+
+        byte[] content = parser.getContent();
+        int i = 0;
+        for (byte b: content) {
+            if (i % 16 == 0) {
+                System.out.println();
+            }
+            System.out.printf("%02x ", b);
+            i++;
+        }
+
+        // выводит файл в utf-8
+        // parser.getContent1();
+
+    }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Parser {
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Parser {
+
+    private File file;
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public byte[] getContent() {
+        byte[] buffer = null;
+        try (InputStream inputStream = new FileInputStream(file)) {
+            buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+        }catch (IOException e) {
+            e.getStackTrace();
+        }
+        return buffer;
+    }
+
+    public void getContent1() {
+        try (InputStream fis = new FileInputStream(file);
+              InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+              BufferedReader br = new BufferedReader(isr)) {
+            br.lines().forEach(line -> System.out.println(line));
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+
+    }
+
+
 }
